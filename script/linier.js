bde9d75dfa187aa1d67900b57a85e8bf1bf6d2dd
Ridwan <pratamaridwan111@gmail.com>
2021-02-18 17:58:13 +0700

    pertidaksamaan linier

--- script/linier.js
+++ script/linier.js
@@ -1,49 +1,31 @@
-//fungsi untuk menghitung akar kuadrat
-function quadraticRoots(a,b,c){
+function quadraticRoots(a,b,c, equivalent){
 	const roots = document.getElementById("roots");
-	const D = a * c + a * b;
-    const C = (a*b) * c;
-    const root = Math.sqrt(D*D - 4*a*C);
-    const disk = (b*b - 4*a*c);
-    
-    // let x1 = (-D + root) / (2*a);
-	// let x2 = (-D - root) /(2*a);
+	const hasil = (c-b)/a;
 
-    // let ans = "Karena D = " + D + ", maka akar - akarnya adalah " + x1 + " dan " + x2;
-	// roots.innerHTML = ans;
+    if(a <0 && equivalent == "<"){
+        equivalent = ">";
+    }
+    else if(a <0 && equivalent == ">"){
+        equivalent = "<";
+    }
+    else if(a <0 && equivalent == "<="){
+        equivalent = ">=";
+    }
+    else if(a <0 && equivalent == ">="){
+        equivalent = "<=";
+    }
 
-	if(a != 0){
-		if(disk > 0){
-			//dua akar penyelesaian
-			let x1 = (-D + root) / (2*a);
-	        let x2 = (-D - root) /(2*a);
-			// if(Math.sqrt(D) % 1 != 0){
-			// 	x1 = x1.toFixed(2);
-			// 	x2 = x2.toFixed(2);
-			// }
-			let ans = "Karena D = " + D + ", maka akar - akarnya adalah " + x1 + " dan " + x2;
-			roots.innerHTML = ans;
+    // console.log(hasil);
+    console.log(equivalent);
 
-		}
-		else if(disk == 0){
-			//akar penyelesaian sama
-			let x = -b/2*a;
-			let ans = "Karena D = " + D + ", maka akarnya adalah " + x;
-			roots.innerHTML = ans;
-		}
-		else{
-			//akar imajiner
-            // MASUK KESINI WAE
-			roots.innerHTML = "Karena D < 0, maka akar - akarnya merupakan akar - akar imajiner!";
-		}
-	}
-	else{
-		//bukan persamaan kuadrat
-	}
+    let ans = hasil + "<br>" + equivalent;
+    roots.innerHTML = ans;
+
+   
 }
 
 //fungsi untuk mengubah bentuk persamaan kuadrat
-function createQuadraticStr(a,b,c,str){
+function createQuadraticStr(a,b,c,equivalent,str){
 	//untuk a
 	if(a == 1){
 		//hilangkan angkanya
@@ -61,7 +43,7 @@ function createQuadraticStr(a,b,c,str){
 	//untuk b
 	if(b == 0){
 		//dihilangkan tanda + dan x
-		str = str.replace("+ bx","");
+		str = str.replace("+ b","");
 	}
 	else if(b == 1){
 		//dihilangkan angkanya
@@ -84,7 +66,7 @@ function createQuadraticStr(a,b,c,str){
 
 	//untuk c
 	if(c == 0){
-		str = str.replace("+ c","");
+		str = str.replace("c", c);
 	}
 	else if(c < 0){
 		str = str.replace("+ c","- "+(c/-1));
@@ -92,6 +74,21 @@ function createQuadraticStr(a,b,c,str){
 	else{
 		str = str.replace("c",c);
 	}
+
+	// replace equivalent
+	if(equivalent == "<"){
+		str = str.replace(">", "<");
+	}
+	else if(equivalent == ">"){
+		str = str.replace(">", ">");
+	}
+	else if(equivalent == ">="){
+		str = str.replace(">", ">=");
+	}
+	else if(equivalent == "<="){
+		str = str.replace(">", "<=");
+	}
+
 
 	return str;
 }
@@ -106,24 +103,24 @@ function find(){
 	let a = parseInt(document.getElementById("a").value);
 	let b = parseInt(document.getElementById("b").value);
 	let c = parseInt(document.getElementById("c").value);
+	let equivalent = document.getElementById("equivalent").value;
 
 	//validasi input dari user
 	//jika bukan angka, beri alert
 	const notif = document.getElementById("notif");
-	if(validateInput(a,b,c)){
-		//jika a bukan 0, maka merupakan pers. kuadrat
+	if(validateInput(a,b,c, equivalent)){
 		if(a != 0){
 			//deklarasi variabel
 			const quadraticEq = document.getElementById("quadratic_equation");
 			const answer = document.getElementById("answer");
-			let quadratic_str = "ax<sup>2</sup> + bx + c = 0";
+			let quadratic_str = "ax + b > c";
 
 			//mengganti tampilan pers. kuadrat di jawaban
-			quadratic_str = createQuadraticStr(a,b,c,quadratic_str);
+			quadratic_str = createQuadraticStr(a,b,c, equivalent,quadratic_str);
 			quadraticEq.innerHTML = quadratic_str;
 
 			//mencari akar pers. kuadrat
-			quadraticRoots(a,b,c);
+			quadraticRoots(a,b,c, equivalent);
 
 			//menampilkan jawaban
 			answer.style.display = "block";
@@ -148,7 +145,6 @@ function reset(){
 	document.getElementById("b").value = "";
 	document.getElementById("c").value = "";
 	answer.style.display = "none";
-	document.getElementById("quadratic_equation").innerHTML = "ax<sup>2</sup> + bx + c = 0";
 }
 
 
