560316292945a2888f6175e53adfeacc5ade315f
Restuaji Eliansyah <restuaerestu291@gmail.com>
2021-02-25 21:17:37 +0700

    penyelesaian faktor

--- script/kuadrat.js
+++ script/kuadrat.js
@@ -1,39 +1,74 @@
 //fungsi untuk menghitung akar kuadrat
 function quadraticRoots(a,b,c){
 	const roots = document.getElementById("roots");
-	const D = a * c + a * b;
-    const C = (a*b) * c;
-    const root = Math.sqrt(D*D - 4*a*C);
-    const disk = (b*b - 4*a*c);
-    
-    // let x1 = (-D + root) / (2*a);
-	// let x2 = (-D - root) /(2*a);
-
-    // let ans = "Karena D = " + D + ", maka akar - akarnya adalah " + x1 + " dan " + x2;
-	// roots.innerHTML = ans;
-
+	const D = b**2 - 4*a*c;
+	let penyelesaian = "";
+	let charA = "";
+	let charB = "";
 	if(a != 0){
-		if(disk > 0){
+		if(D > 0){
 			//dua akar penyelesaian
-			let x1 = (-D + root) / (2*a);
-	        let x2 = (-D - root) /(2*a);
-			// if(Math.sqrt(D) % 1 != 0){
-			// 	x1 = x1.toFixed(2);
-			// 	x2 = x2.toFixed(2);
-			// }
-			let ans = "Karena D = " + D + ", maka akar - akarnya adalah " + x1 + " dan " + x2;
+			let x1 = (-b + Math.sqrt(D))/(2*a);	
+			let x2 = (-b - Math.sqrt(D))/(2*a);
+			
+			if(Math.sqrt(D) % 1 != 0){
+				x1 = x1.toFixed(2);
+				x2 = x2.toFixed(2);
+			}
+			if(a==1){
+			if(x1 > 0){
+				charA = -x1+")";
+			}
+			else{
+				charA = "+"+ -x1+")";
+			}
+		} else if(a==-1){
+			if(x1 > 0){
+				charA = "+"+ -x1+")";
+			}
+			else{
+				charA = -x1+")";
+			}
+		} else if(a<-1){
+			if(x1 > 0){
+				charA = a*x1+")";
+			}
+			else{
+				charA = "+"+ a*x1+")";
+			}
+		}
+		else{
+			if(x1 > 0){
+				charA = -a*x1+")";
+			}
+			else{
+				charA = "+"+ -a*x1+")";
+			}
+		}
+			if(x2 > 0){
+				charB = -x2+")";
+			}
+			else{
+				charB = "+"+ -x2+")";
+			}
+			if(a != 1){
+				penyelesaian = "("+a+"x " + charA + "(x " + charB;	
+			}
+			else{
+			penyelesaian = "(x " + charA + "(x " + charB;
+			}
+			let ans = penyelesaian + "<br>Karena D = " + D + ", maka akar - akarnya adalah " + x1 + " dan " + x2;
 			roots.innerHTML = ans;
 
 		}
-		else if(disk == 0){
+		else if(D == 0){
 			//akar penyelesaian sama
-			let x = -b/2*a;
+			let x = (-b + Math.sqrt(D))/(2*a);
 			let ans = "Karena D = " + D + ", maka akarnya adalah " + x;
 			roots.innerHTML = ans;
 		}
 		else{
 			//akar imajiner
-            // MASUK KESINI WAE
 			roots.innerHTML = "Karena D < 0, maka akar - akarnya merupakan akar - akar imajiner!";
 		}
 	}
